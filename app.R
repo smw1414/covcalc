0648f4ecc8f56d2e80cb8496483749271d26ceae
smw1414 <wsm147@outlook.com>
2018-01-12 17:02:30 +0800

    add some corrections

--- app.R
+++ app.R
@@ -21,7 +21,8 @@ ui <- fluidPage(
 
    HTML("<p><strong>Instructions:</strong> set the read length/configuration and genome size, then select what you want to calculate.</p>"),
 
-   HTML("<p>Written by <a href='http://stephenturner.us', target='blank'>Stephen Turner</a>, based on the <a href='http://www.ncbi.nlm.nih.gov/pubmed/3294162' target='_blank'>Lander-Waterman formula</a>, inspired by <a href='http://core-genomics.blogspot.com/2016/05/how-many-reads-to-sequence-genome.html' target='_blank'>a similar calculator</a> written by James Hadfield. Coverage is calculated as <em>C=LN/G</em> and reads as <em>N=CG/L</em> where <em>C</em> = Coverage (X), <em>L</em> = Read length (bp), <em>G</em> = Haploid genome size (bp), and <em>N</em> = Number of reads. Source code <a href='https://github.com/stephenturner/covcalc' target='_blank'>on GitHub</a>.</p>"),
+   HTML("<p>Written by <a href='http://stephenturner.us', target='blank'>Stephen Turner</a>, based on the <a href='http://www.ncbi.nlm.nih.gov/pubmed/3294162' target='_blank'>Lander-Waterman formula</a>, inspired by <a href='http://core-genomics.blogspot.com/2016/05/how-many-reads-to-sequence-genome.html' target='_blank'>a similar calculator</a> written by James Hadfield.<br/><em>C</em> = Coverage (X), <em>L</em> = Read length (bp), <em>G</em> = Haploid genome size (bp), <em>N</em> = Number of reads,  <em>ON</em> = On-target percentage and  <em>Dup</em> = Duplication percentage 
+        <br/>Coverage is calculated as <em>C=LN/G</em> and reads as <em>N=CG/L/ON/Dup</em>. <br/>Forked from <a href='https://github.com/stephenturner/covcalc' target='_blank'>on GitHub</a>.</p>"),
 
    # HTML("<p>Written by <a href='http://stephenturner.us', target='blank'>Stephen Turner</a>, based on the <a href='http://www.ncbi.nlm.nih.gov/pubmed/3294162' target='_blank'>Lander-Waterman formula</a>, inspired by <a href='http://core-genomics.blogspot.com/2016/05/how-many-reads-to-sequence-genome.html' target='_blank'>a similar calculator</a> written by James Hadfield.</p><p>Coverage is calculated as <em>C=LN/G</em> and reads as <em>N=CG/L</em> where
    #        <ul>
@@ -36,7 +37,9 @@ ui <- fluidPage(
    sidebarLayout(
       sidebarPanel(
 
-        numericInput("readlength", label = h4("Read length (bp)"), value = 125),
+        numericInput("readlength", label = h4("Read length (bp)"), value = 75),
+        numericInput("ontarget", label = h4("Expected on-target (%)"), value = 70),
+        numericInput("dup", label = h4("Expected duplication (%)"), value = 15),
         radioButtons("sepe", label = NA,
                      choices = list("Paired-end" = 2, "Single-end" = 1),
                      selected = 2),
@@ -49,6 +52,7 @@ ui <- fluidPage(
                      choices=list(
                        "Human (3.1 Gb)" = 3096649726,
                        "Agilent V6 exome (60 Mb)" = 60e6,
+                       "TruSeq exome (45 Mb)" = 45e6,
                        "S. cerevisiae (12.2 Mb)" = 12157105,
                        "E. coli K-12 (4.6 Mb)" = 	4641652,
                        "Other (Enter manually)"=""),
@@ -72,8 +76,9 @@ ui <- fluidPage(
              uiOutput("ui")
         ),
 
-        h1(textOutput("text")),
-
+        #h1(textOutput("text")),
+        h1(htmlOutput("text")),
+        
         # plotOutput("myplot"),
 
         tags$br()
@@ -115,7 +120,7 @@ server <- function(input, output) {
       return()
     } else {
       switch(input$whatcalc,
-             "reads"=sliderInput("dynamic", h4("Desired coverage"), min=10, max=150, value=30, step=5, post="x"),
+             "reads"=sliderInput("dynamic", h4("Desired coverage"), min=5, max=500, value=30, step=5, post="x"),
              "coverage"=sliderInput("dynamic",
                                     label=h4("Number reads sequenced (millions)"),
                                     min=1, max=1001, value=384,
@@ -126,9 +131,9 @@ server <- function(input, output) {
 
   # Converts genome size into SI-prefix character
   bptosi <- function(bp) {
-    if (bp>1e9)      return(paste0(round(bp/1e9, 2), "GB"))
-    else if (bp>1e6) return(paste0(round(bp/1e6, 2), "MB"))
-    else if (bp>1e3) return(paste0(round(bp/1e3, 2), "KB"))
+    if (bp>1e9)      return(paste0(round(bp/1e9, 2), "Gb"))
+    else if (bp>1e6) return(paste0(round(bp/1e6, 2), "Mb"))
+    else if (bp>1e3) return(paste0(round(bp/1e3, 2), "Kb"))
     else             return(paste0(bp, "bp"))
   }
 
@@ -140,18 +145,31 @@ server <- function(input, output) {
       paste0("Calculating coverage", input$dynamic)
     }
   })
-
+  
   output$text <- renderText({
-    req(input$whatcalc)
+    #req(input$whatcalc)
     if (input$genomesizebutton=="") req(input$genomesizeOther)
     if (input$whatcalc=="reads") {
       # N=CG/L
-      reads <- (as.numeric(input$dynamic)*as.numeric(genomesize()))/(as.numeric(input$readlength)*as.numeric(input$sepe))
-      paste0("*", signif(reads/1e6, 3), " million reads* required for ", input$dynamic, "X coverage of a ", bptosi(genomesize()), " genome using ", input$sepe, "x", input$readlength, " sequencing reads.")
+      reads <- (as.numeric(input$dynamic)*as.numeric(genomesize()))/(as.numeric(input$readlength)*as.numeric(input$sepe))/as.numeric(input$ontarget*0.01)/as.numeric(1-(input$dup*0.01))
+      gb <- (as.numeric(input$readlength)*as.numeric(input$sepe))*reads/1e+9
+      str1 <- paste0(input$dynamic, "X coverage of ", bptosi(genomesize()), " target region")
+      str2 <- paste0("Reads: ",signif(reads/1e6, 3)," million reads")
+      str3 <- paste0("Read Length: ",input$readlength , " x ", input$sepe)
+      str4 <- paste0("Bases output: ",round(gb,digits = 1),"Gb")
+      HTML(paste(str1, str2, str3, str4, sep = '<br/>'))
+      #reads <- (as.numeric(input$dynamic)*as.numeric(genomesize()))/(as.numeric(input$readlength)*as.numeric(input$sepe))       
+      #paste0("*", signif(reads/1e6, 3), " million reads* required for ", input$dynamic, "X coverage of a ", bptosi(genomesize()), " genome using ", input$sepe, "x", input$readlength, " sequencing reads.")
     } else if (input$whatcalc=="coverage") {
       # C=LN/G
-      coverage <- (as.numeric(input$readlength)*as.numeric(input$sepe))*as.numeric(input$dynamic)*1e6/as.numeric(genomesize())
-      paste0("*", round(coverage), "X coverage* for a ", bptosi(genomesize()), " genome obtained with ", input$dynamic, "M ", input$sepe, "x", input$readlength, " sequencing reads.")
+      coverage <- (as.numeric(input$readlength)*as.numeric(input$sepe))*as.numeric(input$dynamic)*1e6/as.numeric(genomesize())*as.numeric(input$ontarget*0.01)*as.numeric(1-(input$dup*0.01))
+      gb2 <-  ((as.numeric(input$readlength)*as.numeric(input$sepe))*as.numeric(input$dynamic)*1e6)/1e9
+      str1 <- paste0(round(coverage), "X coverage of ", bptosi(genomesize()), " target region")
+      str2 <- paste0("Reads: ",input$dynamic," million reads")
+      str3 <- paste0("Read Length: ",input$readlength , " x ", input$sepe)
+      str4 <- paste0("Bases output: ",round(gb2, digits = 1),"Gb")
+      HTML(paste(str1, str2, str3, str4, sep = '<br/>'))
+      #paste0("*", round(coverage), "X coverage* for a ", bptosi(genomesize()), " genome obtained with ", input$dynamic, "M ", input$sepe, "x", input$readlength, " sequencing reads.")
     }
   })
 
